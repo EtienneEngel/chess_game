68fbf80f87260eb626afc4e72d5afa8afe08a2c4
Etienne Engel <etienne.engel@gmail.com>
2021-02-12 14:30:18 +0000

    Update Pieces.java

---
+++ model/Pieces.java
@@ -0,0 +1,32 @@
+public Interface Pieces{
+    // Methods
+    int getX()
+    {
+        return this.x
+    }
+
+    int getY()
+    {
+        return this.y
+    }
+
+    Color getColor()
+    {
+        return this.color
+    }
+
+    boolean isMoveOk(int x_final, int y_final)
+    {
+        #TODO
+    }
+
+    boolean move(int xFinal, int yFinal)
+    {
+        #TODO
+    }
+
+    boolean capture()
+    {
+        #TODO
+    }
+}
