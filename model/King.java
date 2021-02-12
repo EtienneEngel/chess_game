ebe2c4e603156176ecdd84e6c7abb37ac7b4c26b
Etienne Engel <etienne.engel@gmail.com>
2021-02-12 14:29:34 +0000

    Update King.java

---
+++ model/King.java
@@ -0,0 +1,22 @@
+public class King extends AbstractPiece{
+    // attributs
+    private Color color
+    private Coord x
+    private Coord y
+
+
+    // Constructor
+    public King(Color piece_color, Coord coord)
+    {
+        #TODO
+    }
+
+
+    // Methods
+
+
+    public boolean isMoveOk(int xFinal, int yFinal)
+    {
+        #TODO
+    }
+}
