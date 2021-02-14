be54029f1be7863d75396ab8f6d30989f2a0bc2b
Etienne Engel <etienne.engel@gmail.com>
2021-02-14 20:32:00 +0100

    Start populating classes

--- model/King.java
+++ model/King.java
@@ -1,14 +1,16 @@
+package model;
+
 public class King extends AbstractPiece{
-    // attributs
-    private Color color
-    private Coord x
-    private Coord y
+    // Attributs
+    private Color color;
+    private Coord x;
+    private Coord y;
 
 
     // Constructor
     public King(Color piece_color, Coord coord)
     {
-        #TODO
+        // TODO
     }
 
 
@@ -17,6 +19,6 @@ public class King extends AbstractPiece{
 
     public boolean isMoveOk(int xFinal, int yFinal)
     {
-        #TODO
+        // TODO
     }
 }
